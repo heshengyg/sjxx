95db6533487e87d22872fb76db5c60fc11c192d9
heshengyg <15512469@qq.com>
2026-08-14 10:54:55 +0800

    更新 app.js

--- app.js
+++ app.js
@@ -1528,8 +1528,8 @@ function initStickyHeaders() {
 
     headers.forEach((header, index) => {
         const rect = header.getBoundingClientRect();
-        // ★ 关键：提前触发，让帘头在内容标题即将滚出时出现（偏移量 -50px）
-        const top = rect.top + scrollY - 150;
+        // ★ 关键：提前触发，让帘头在内容标题即将滚出时出现（偏移量 -250px）
+        const top = rect.top + scrollY -250;
         const next = headers[index + 1];
         const nextTop = next ? next.getBoundingClientRect().top + scrollY : dashboard.scrollHeight;
         headerList.push({
